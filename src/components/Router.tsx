a88ce9d11b5b861849615ca29d71e643b09dfcf4
nohj0518 <nohj0518@daum.net>
2024-10-06 17:51:55 +0900

    set routers

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -4,6 +4,12 @@ import PostListPage from "pages/posts";
 import PostDetail from "pages/posts/detail";
 import PostNew from "pages/posts/new";
 import PostEdit from "pages/posts/edit";
+import ProfilePage from "pages/profile";
+import ProfileEdit from "pages/profile/edit";
+import NotificationsPage from "pages/notifications";
+import SearchPage from "pages/search";
+import LoginPage from "pages/users/login";
+import SignUpPage from "pages/users/signup";
 export default function Router() {
   return (
     <Routes>
@@ -12,12 +18,12 @@ export default function Router() {
       <Route path="/posts/:id" element={<PostDetail />}></Route>
       <Route path="/posts/new" element={<PostNew />}></Route>
       <Route path="/posts/edit/:id" element={<PostEdit />}></Route>
-      <Route path="/profile" element={<>profile page</>}></Route>
-      <Route path="/profile/edit" element={<>profile edit page</>}></Route>
-      <Route path="/notifications" element={<>notification page</>}></Route>
-      <Route path="/search" element={<>search page</>}></Route>
-      <Route path="/users/login" element={<>login page</>}></Route>
-      <Route path="/users/signup" element={<>signup page</>}></Route>
+      <Route path="/profile" element={<ProfilePage />}></Route>
+      <Route path="/profile/edit" element={<ProfileEdit />}></Route>
+      <Route path="/notifications" element={<NotificationsPage />}></Route>
+      <Route path="/search" element={<SearchPage />}></Route>
+      <Route path="/users/login" element={<LoginPage />}></Route>
+      <Route path="/users/signup" element={<SignUpPage />}></Route>
       <Route path="*" element={<Navigate replace to="/" />}></Route>
     </Routes>
   );
