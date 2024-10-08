c3c8a1c02e61975c970d228e6a7135af972f781b
nohj0518 <nohj0518@daum.net>
2024-10-09 00:53:03 +0900

    create loader component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Layout } from "components/Layout";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { app } from "firebaseApp";
 import { ToastContainer } from "react-toastify";
+import Loader from "components/loader/Loader";
 
 function App() {
   const auth = getAuth(app);
@@ -35,8 +36,13 @@ function App() {
   return (
     <>
       <Layout>
-        <ToastContainer />
-        {init ? <Router isAuthenticated={isAuthenticated} /> : "loading"}
+        <ToastContainer
+          theme="dark"
+          autoClose={1000}
+          hideProgressBar
+          newestOnTop
+        />
+        {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
       </Layout>
     </>
   );
