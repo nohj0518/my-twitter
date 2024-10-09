67bba994cfbef63c478d15b4fb535332fcfdf747
nohj0518 <nohj0518@daum.net>
2024-10-09 21:04:12 +0900

    feat get detail post

--- src/pages/posts/detail.tsx
+++ src/pages/posts/detail.tsx
@@ -1,3 +1,39 @@
+import { useCallback, useState } from "react";
+import { PostProps } from "pages/home";
+import PostBox from "components/posts/PostBox";
+import Loader from "components/loader/Loader";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "firebaseApp";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect } from "react";
+
+import { IoIosArrowBack } from "react-icons/io";
+
 export default function PostDetail() {
-  return <>PostDetailPage page</>;
+  const [post, setPost] = useState<PostProps | null>(null);
+  const navigate = useNavigate();
+  const params = useParams();
+
+  const getPost = useCallback(async () => {
+    if (params.id) {
+      const docRef = doc(db, "posts", params.id);
+      const docSnap = await getDoc(docRef);
+      setPost({ ...docSnap.data(), id: docSnap.id } as PostProps);
+    }
+  }, [params.id]); // params.id 가 있을때만 useCallback 실행됨
+  useEffect(() => {
+    if (params.id) getPost();
+  }, [getPost, params.id]);
+  return (
+    <>
+      <div className="post">
+        <div className="post__header">
+          <button type="button" onClick={() => navigate(-1)}>
+            <IoIosArrowBack className="post__header-btn" />
+          </button>
+        </div>
+        {post ? <PostBox post={post} /> : <Loader />}
+      </div>
+    </>
+  );
 }
