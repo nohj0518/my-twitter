a54b0b6b26514156105fc2c57f9ba322065d8f18
nohj0518 <nohj0518@daum.net>
2024-10-09 17:40:59 +0900

    feat addDoc posting

--- src/components/posts/PostForm.tsx
+++ src/components/posts/PostForm.tsx
@@ -1,16 +1,51 @@
 import { FiImage } from "react-icons/fi";
+import { useContext, useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "firebaseApp";
+import AuthContext from "context/AuthContext";
+import { toast } from "react-toastify";
 export default function PostForm() {
+  const [content, setContent] = useState<string>("");
+  const { user } = useContext(AuthContext);
   const handleFileUpload = () => {};
+  const onSubmit = async (e: any) => {
+    e.preventDefault();
+
+    try {
+      await addDoc(collection(db, "posts"), {
+        content: content,
+        createdAt: new Date()?.toLocaleDateString("ko", {
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit",
+        }),
+        uid: user?.uid,
+        email: user?.email,
+      });
+      setContent("");
+      toast.success("게시글을 생성하였습니다.");
+    } catch (e: any) {}
+  };
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "content") {
+      setContent(value);
+    }
+  };
   return (
     <>
       {/** Post Form */}
-      <form className="post-form">
+      <form className="post-form" onSubmit={onSubmit}>
         <textarea
           className="post-form__textarea"
           required
           name="content"
           id="content"
           placeholder="What is happening"
+          onChange={onChange}
+          value={content}
         />
         <div className="post-form__submit-area">
           <label htmlFor="file-input" className="post-form__title">
--- src/firebaseApp.ts
+++ src/firebaseApp.ts
@@ -1,4 +1,5 @@
 import { initializeApp, FirebaseApp, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 export let app: FirebaseApp;
 /**
@@ -29,5 +30,7 @@ try {
 }
 
 const firebase = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
 
 export default firebase;
