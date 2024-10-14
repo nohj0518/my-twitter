add99284aaff68db8521f1586803e6e5b05a7f31
nohj0518 <nohj0518@daum.net>
2024-10-14 23:14:33 +0900

    feat comments

--- src/pages/posts/detail.tsx
+++ src/pages/posts/detail.tsx
@@ -2,12 +2,14 @@ import { useCallback, useState } from "react";
 import { PostProps } from "pages/home";
 import PostBox from "components/posts/PostBox";
 import Loader from "components/loader/Loader";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, onSnapshot } from "firebase/firestore";
 import { db } from "firebaseApp";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 
 import PostHeader from "components/posts/PostHeader";
+import CommentForm from "components/comments/CommentForm";
+import CommentBox, { CommentProps } from "components/comments/CommentBox";
 
 export default function PostDetail() {
   const [post, setPost] = useState<PostProps | null>(null);
@@ -16,8 +18,9 @@ export default function PostDetail() {
   const getPost = useCallback(async () => {
     if (params.id) {
       const docRef = doc(db, "posts", params.id);
-      const docSnap = await getDoc(docRef);
-      setPost({ ...docSnap.data(), id: docSnap.id } as PostProps);
+      onSnapshot(docRef, (doc) => {
+        setPost({ ...doc?.data(), id: doc?.id } as PostProps);
+      });
     }
   }, [params.id]); // params.id 가 있을때만 useCallback 실행됨
   useEffect(() => {
@@ -27,7 +30,20 @@ export default function PostDetail() {
     <>
       <div className="post">
         <PostHeader />
-        {post ? <PostBox post={post} /> : <Loader />}
+        {post ? (
+          <>
+            <PostBox post={post} />
+            <CommentForm post={post} />
+            {post?.comments
+              ?.slice(0)
+              ?.reverse()
+              ?.map((data: CommentProps, index: number) => (
+                <CommentBox data={data} key={index} post={post} />
+              ))}
+          </>
+        ) : (
+          <Loader />
+        )}
       </div>
     </>
   );
